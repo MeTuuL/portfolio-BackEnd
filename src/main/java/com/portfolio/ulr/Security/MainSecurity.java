b74ee266f1708ab0e7b33465a64a582e5837ab9c
Daniel Ulrich <121643833+MeTuuL@users.noreply.github.com>
2023-05-15 11:35:01 -0300

    Se siguen corrigiendo complicaciones

--- src/main/java/com/portfolio/ulr/Security/MainSecurity.java
+++ src/main/java/com/portfolio/ulr/Security/MainSecurity.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -58,8 +57,4 @@ public class MainSecurity {
         return http.build();
     }
     
-    @Bean
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-       auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder());
-    }
 }
