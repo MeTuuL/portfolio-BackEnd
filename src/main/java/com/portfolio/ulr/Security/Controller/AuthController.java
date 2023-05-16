f44f6facd97a58b9085f02586c00f9fe692e0a0b
Daniel Ulrich <121643833+MeTuuL@users.noreply.github.com>
2023-05-15 21:41:03 -0300

    Otro intento de solucion de /auth

--- src/main/java/com/portfolio/ulr/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/ulr/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://danportfolio-frontend.web.app"})
+@CrossOrigin(origins = "**")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
